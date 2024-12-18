940b6358f57299369d4f6569774457582e096446
Bohdan <bohdan@neuralriver.ai>
2024-12-18 20:00:42 +0200

    Added header section; Changed social list items to link; Added mobile/desktop menu;

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,17 @@
   const menuRefs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    overlay: document.querySelector('[data-menu-overlay]'),
     menu: document.querySelector('[data-menu]'),
   }
 
   menuRefs.openMenuBtn?.addEventListener('click', toggleMenu)
   menuRefs.closeMenuBtn?.addEventListener('click', toggleMenu)
+  menuRefs.overlay?.addEventListener('click', toggleMenu)
 
   function toggleMenu() {
     menuRefs.menu?.classList.toggle('is-open')
+    menuRefs.overlay?.classList.toggle('is-open')
   }
 
   const header = document.querySelector('[data-header]')
@@ -21,4 +24,22 @@
       header?.classList.remove('scrolled')
     }
   })
+
+  const menuItems = document.querySelectorAll('.navigation-link');
+
+  function setActiveMenu() {
+    const currentHash = window.location.hash;
+
+    menuItems.forEach((item) => {
+      console.log(item, currentHash);
+      if (item.getAttribute('href') === currentHash) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
+  window.addEventListener('hashchange', setActiveMenu);
+  window.addEventListener('load', setActiveMenu);
 })()
